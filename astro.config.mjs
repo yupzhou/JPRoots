441943c6671d4e2ce203f91de1c798e24fbc44c0
yupzhou <57206007+yupzhou@users.noreply.github.com>
2026-05-18 01:43:44 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,6 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	site: 'https://yupzhou.github.io',
 	base: '/JPRoots',
-	build: {
-    assets: '_astro',
-    format: 'directory',
-  },
 	integrations: [
 		starlight({
 			title: 'JPRoots',
