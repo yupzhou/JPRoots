e25f58f20375bcb4cd8d679636567aed1e6dd135
yupzhou <57206007+yupzhou@users.noreply.github.com>
2026-05-18 02:01:24 +0800

    260429 v0.9 back

---
+++ assets/js/others_sta.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?1e47f291de997968c1d4066dc4dd6782";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
