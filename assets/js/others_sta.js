f3d277c8753b40b7acb6cd18ef5a04139d4a2866
yupzhou <57206007+yupzhou@users.noreply.github.com>
2026-05-17 23:45:17 +0800

    260517 v0.9 delete

--- assets/js/others_sta.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?1e47f291de997968c1d4066dc4dd6782";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
