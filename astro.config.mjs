2bf05b4200d2f9e8da587ed494ac71cfc950f6b1
yupzhou <57206007+yupzhou@users.noreply.github.com>
2026-05-18 18:59:16 +0800

    260518 v0.9b online test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://yupzhou.github.io',
-	base: '/JPRoots',
+	site: 'https://yupzhou.github.io/JPRoots',
+	base: '',
 	integrations: [
 		starlight({
 			title: 'JPRoots',
